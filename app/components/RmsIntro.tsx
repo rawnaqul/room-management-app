a5f7304c4f0f9fdf39e8ae8db001ce72940d5106
rawnaqul <slow.coach.rawnaq@gmail.com>
2024-06-11 18:20:34 +0600

    laptop synced

--- app/components/RmsIntro.tsx
+++ app/components/RmsIntro.tsx
@@ -14,15 +14,9 @@ const RmsIntro = () => {
                 <h2 className='text-4xl my-4 text-left'>Start your journey with us</h2>
                 <p className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry-s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
             </div>
-<<<<<<< HEAD
             <div className=' flex flex-row justify-between px-6 bg-green-3000'>
                 <Button className='px-4 py-1 bg-emerald-400 rounded-full'>Next</Button>
                 <Button className='px-4 py-1 bg-yellow-400 rounded-full text-emerald-800'>Next</Button>
-=======
-            <div className='mt-40 flex flex-row justify-between px-6'>
-                <Button className='px-4 py-1 bg-emerald-400 rounded-full'>Next</Button>
-                <Button className='px-4 py-1 bg-yellow-400 rounded-full text-emerald-800'>Skip</Button>
->>>>>>> 8199fdae686adc82a9a3931b86d6dded1ea79b94
             </div>
         </div>
     )
